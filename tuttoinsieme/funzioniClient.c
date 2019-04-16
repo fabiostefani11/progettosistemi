4407ce0ce7c7878137d03c1dfb8d80e70d477d06
Luca Mori <luca.mori@student.unife.it>
2019-04-16 12:01:15 +0200

    json vari

---
+++ tuttoinsieme/funzioniClient.c
@@ -0,0 +1,9 @@
+#include "client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
---
+++ tuttoinsieme/server.h
@@ -0,0 +1,38 @@
+#define PROTOPORT 5193 //numero della porta di default
+#define QLEN 6         //grandezza della coda
+#define BUFFERSIZE 256
+
+#define DIM 256
+#define dim 10
+
+typedef struct
+{
+    char parola[DIM];
+    int ID;
+    int ombrellone;
+    int fila;
+    int data_inizio;
+    int data_fine;
+    int nparole;
+
+} messaggio;
+
+typedef struct
+{
+    int ID;
+    int numero;
+    int fila;
+    int disponibile; //0=disponibile 1=occupato adesso 2=occupato in futuro 3=occupato sia adesso che in futuro 4=temporaneamente occupato
+
+} ombrellone;
+
+typedef struct
+{
+    ombrellone Ombrellone[DIM];
+    char msg[DIM];
+    int ombrelloni_liberi;
+} risposta;
+
+int uniscidata(char data[]);
+messaggio dividiFrase(char msg[]);
+risposta elaboraRisposta(risposta Risposta, messaggio Messaggio);

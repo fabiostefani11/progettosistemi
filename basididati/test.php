2280ccbf6efbd95af083a0a160a3d2ae751eaf61
Luca Mori <luca.mori@student.unife.it>
2019-05-23 10:47:14 +0200

    sadda

---
+++ basididati/test.php
@@ -0,0 +1,12 @@
+<?php
+$Nome = $_POST['Nome'];
+$Cognome = $_POST['Cognome'];
+?>
+<html>
+    <head>
+        <title>test.php</title>
+</head>
+<body>
+    <p>Ciao <?php echo $Nome." ". $Cognome; ?> benvenuto nel server </p>
+</Body>
+</html>

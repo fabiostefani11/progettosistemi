4495d11f2df5678ec8516a0d54dd530e542d9ebd
Luca Mori <luca.mori@student.unife.it>
2019-06-04 09:29:10 +0200

    asas

---
+++ datadioggi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t data;
+    int giorno, mese, anno;
+    time(&data);
+    struct tm *local = localtime(&data);
+    giorno = local->tm_mday;
+    mese = local->tm_mon + 1;
+    anno = local->tm_year + 1900;
+    printf("%02d/%02d/%d\n", giorno, mese, anno);
+}
